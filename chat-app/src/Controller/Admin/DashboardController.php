c63c196631599c81769714b62110dfdac6882ad5
nhan.nguyen <truongnhan0311@gmail.com>
2023-10-06 17:48:57 +0700

    add webpack

--- chat-app/src/Controller/Admin/DashboardController.php
+++ chat-app/src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -43,6 +44,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', App\Entity\User::class);
+        yield MenuItem::linkToCrud('The Label', 'fas fa-list', User::class);
     }
 }
