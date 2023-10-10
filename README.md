9cf9afdb87beb1488f31607f95fbc97a3b9ca9bb
nhan.nguyen <truongnhan0311@gmail.com>
2023-10-10 09:05:27 +0700

    add registration form

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mercure-chat
+
+
+1) Install some missing packages:
+   composer require symfonycasts/verify-email-bundle
+2) In RegistrationController::verifyUserEmail():
+    * Customize the last redirectToRoute() after a successful email verification.
+    * Make sure you're rendering success flash messages or change the $this->addFlash() line.
+3) Review and customize the form, controller, and templates as needed.
+4) Run "php bin/console make:migration" to generate a migration for the newly added User::isVerified property.
+
--- chat-app/src/Entity/User.php
+++ chat-app/src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -31,6 +33,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\ManyToMany(targetEntity: Pizza::class, mappedBy: 'PizzaOfUser')]
     private Collection $pizzas;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -130,6 +135,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         if ($this->pizzas->removeElement($pizza)) {
             $pizza->removePizzaOfUser($this);
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
