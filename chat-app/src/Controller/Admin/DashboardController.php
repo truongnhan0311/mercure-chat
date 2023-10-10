6b4814b85d416964f09c558f9ff278279f9fc020
nhan.nguyen <truongnhan0311@gmail.com>
2023-10-10 08:50:32 +0700

    link pizza to dashboard

--- chat-app/src/Controller/Admin/DashboardController.php
+++ chat-app/src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Pizza;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -44,6 +45,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('The Users', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud(" The Pizza", 'fas fa-list', Pizza::class);
     }
 }
